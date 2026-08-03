b73e748c3bb4fbe49b68d2c097bbc6b329611b28
INÁCIO LAYONO DE SOUSA GONÇA <25163066@edu119.local>
2026-08-03 11:25:44 -0300

    Construção inicial do código Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,17 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-  @override
+ @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Calculador de Média',
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+        useMaterial3: true,
+      ),
       home: MediaEscolarPage(),
-      );
+    ); // MaterialApp
   }
 }
 
@@ -24,6 +29,16 @@ class MediaEscolarPage extends StatefulWidget{
 
 }
 class _MediaEscolarPageState extends State<MediaEscolarPage> {
+
+  final TextEditingController nomeController = TextEditingController();
+  final TextEditingController nota1Controller = TextEditingController();
+  final TextEditingController nota2Controller = TextEditingController();
+  final TextEditingController nota3Controller = TextEditingController();
+
+  String nomeAluno = '';
+  String situacao = '';
+  double media = 0;
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -31,9 +46,29 @@ class _MediaEscolarPageState extends State<MediaEscolarPage> {
         title: const Text('Calculador de Média'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text('Minha primeira tela'),
-      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Icon(
+              Icons.school,
+              size: 100
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Média Escolar',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.bold
+              ),
+            )
+          ],
+        )
+        
+      )
     );
+    
   }
 }
